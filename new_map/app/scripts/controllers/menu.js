2e151620925954ec71dd446d54609a8fb2dd8f3d
Samuel Matis <samuel@matiss.sk>
2014-05-01 10:07:20 +0200

    Add menu

---
+++ new_map/app/scripts/controllers/menu.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('withloveApp')
+    .controller('MenuCtrl', function($scope, $rootScope, categoriesService) {
+        $scope.categories = [];
+
+        var categoriesPromise = categoriesService.getCategories();
+        categoriesPromise.then(function(categories) {
+            $scope.categories = categories.data;
+        });
+
+        $scope.filterCategory = function(name, event) {
+            $rootScope.$emit('filterCategory', name, event);
+        }
+    });
---
+++ new_map/app/scripts/directives/menu.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('withloveApp')
+    .directive('appMenu', function() {
+        return {
+            templateUrl: 'views/navigation.tpl.html',
+            replace: true,
+            controller: 'MenuCtrl',
+            link: function(scope, element, attrs) {
+                // Menu animation on hover
+                element.bind('mouseenter', function() {
+                    $(angular.element(element)).stop().animate({
+                        width: "280px"
+                    }, 200);
+                });
+                element.bind('mouseleave', function() {
+                    $(angular.element(element)).stop().animate({
+                        width: "116px"
+                    }, 200);
+                });
+            }
+        };
+    });
