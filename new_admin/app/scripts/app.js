797b225db95107209b0df18416b35ac12e3602fd
Samuel Matis <samuel@matiss.sk>
2014-05-22 18:13:51 +0200

    Add basic constants and login routes

--- new_admin/app/scripts/app.js
+++ new_admin/app/scripts/app.js
@@ -6,6 +6,28 @@ angular.module('withlove.admin', [
     'ngSanitize',
     'ngRoute'
 ])
-.config(function () {
+.constant('apiKey', '')
+.constant('baseUrl', 'api/')
+.constant('mapTypes', {
+    'defaultMap': 'chiwo.geid1fd8',
+    'morningMap': 'chiwo.h78l0k1o',
+    'dayMap': 'chiwo.geid1fd8',
+    'eveningMap': 'chiwo.h78k8i58',
+    'nightMap': 'chiwo.geg7cd6d'
+})
+.config(function ($routeProvider, $httpProvider) {
+
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+    $routeProvider
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'LoginCtrl',
+            authenticate: false
+        })
+        .otherwise({
+            redirectTo: '/login'
+        })
 
 });
