4b237163c952de38f45303b249634193c4e1f5ec
freezy <marek.viger@gmail.com>
2014-05-24 20:32:50 +0200

    Fixed Cross-Origin Resource Sharing

--- api/app/config/app.php
+++ api/app/config/app.php
@@ -106,6 +106,8 @@ return array(
         'Illuminate\Validation\ValidationServiceProvider',
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
+
+        'Barryvdh\Cors\CorsServiceProvider', //barryvdh/laravel-cors
     ),
 
     /*
