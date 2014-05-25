ca0cd0399a4272894b7f3d8522c9054aed1c95e7
freezy <marek.viger@gmail.com>
2014-05-25 09:57:25 +0200

    Added Laravel IDE Helper Generator

--- api/app/config/app.php
+++ api/app/config/app.php
@@ -107,6 +107,7 @@ return array(
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
 
+        // Required packages
         'Barryvdh\Cors\CorsServiceProvider', //barryvdh/laravel-cors
     ),
 
