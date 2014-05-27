5ab8b117315653067e1606ed0e611a10997566c3
Samuel Matis <samuel@matiss.sk>
2014-05-27 15:20:59 +0200

    Fix selectStairs directive

--- new_admin/app/scripts/directives/selectStairs.js
+++ new_admin/app/scripts/directives/selectStairs.js
@@ -3,56 +3,64 @@
 angular.module('withlove.admin')
     .directive('selectStairs', function () {
         return {
-            restrict: 'AE',
-            templateUrl: 'views/selectBox.html',
-            replace: true,
-            require: 'ngModel',
-            scope: {
-                selectboxOptions: '=stairs',
-                defaultOption: '=defaultoption',
-                selectboxValue: '=ngModel'
-            },
-            link: function() {
+            restrict: 'A',
 
-                angular.element('.selected-category').click(function() {
-                    var category = angular.element(this).attr('data-category');
-                    var selected = angular.element('.category.' + category);
+            link: function (scope, element, attrs) {
 
-                    angular.element('.category-picker').prepend(selected);
+                scope.form.category = 1;
 
-                    fadeLI(angular.element('.category'), 0, angular.element('.category').length);
+                element.find('.name-box').click(function(e) {
 
-                    angular.element('.category').bind('click', function() {
-                        var element = angular.element('.selected-category');
-                        var text = angular.element(this).text();
-                        var elementStyle = angular.element(this).attr('style');
+                    $('.category').bind('click', function() {
+
+                        var element = $('.name-box');
 
                         element.attr('class', '')
-                            .addClass('selected-category')
-                            .addClass(angular.element(this).attr('data-category'))
-                            .attr('data-category', angular.element(this).attr('data-category'))
-                            .attr('style', elementStyle);
-                        angular.element('.category-inside', element).text(text);
+                            .addClass('name-box')
+                            .addClass($(this).data('category'))
+                            .attr('data-category', $(this).data('category'));
 
-                        fadeLO(angular.element('.category'), 0, angular.element('.category').length);
+                        fadeLO($('.category'), 0, $('.category').length);
+
+                        scope.form.category = $(this).data('category-id');
+                        scope.editPlace.selectedCategory = $(this).data('category-id');
                     });
+
+
+                    if (e.target !== this) {
+                        e.stopPropagation();
+                    }
+                    else {
+                        var category = $(this).attr('data-category');
+
+                        var selected = $('.category.'+category);
+                        $('.category-picker').prepend(selected);
+
+                        fadeLI($('.category'), 0, $('.category').length);
+                    }
+
+
+
                 });
 
                 function fadeLI(elem, i, length) {
                     if( i < length ) {
                         elem.eq(i).fadeIn(100, function() { fadeLI(elem, i+1, length); });
-                    } else {
-                        return false;
                     }
+                    else return false;
+
                 }
 
                 function fadeLO(elem, i, length) {
                     if( i < length ) {
                         elem.eq(length-1).fadeOut(100, function() { fadeLO(elem, 0, length-1); });
-                    } else {
-                        return false;
                     }
+                    else return false;
+
                 }
+
+
+
             }
         };
     });
