bebcd0c22ba2da5a2f7fb9beb3a6e2dbbb93e8fc
samuelmatis <samuel@matiss.sk>
2014-06-14 12:37:53 +0200

    Improve search

--- map/app/scripts/controllers/search.js
+++ map/app/scripts/controllers/search.js
@@ -1,156 +1,52 @@
 'use strict';
 
 angular.module('withloveApp')
-    .controller('SearchCtrl', function($scope, $modal, $log, $filter, placesService, categoriesService) {
+    .controller('SearchCtrl', function($scope, categoriesService) {
         $scope.form = {};
-        $scope.defaultCategory = {name: 'Select category', color: '#C4C4C4', slug: 'select-category'};
+        $scope.search = '';
+        $scope.defaultCategory = {
+            name: 'Select category',
+            color: '#C4C4C4',
+            slug: 'select-category'
+        };
         $scope.selectedCategory = $scope.defaultCategory;
         $scope.categories = [];
 
-        var categoriesPromise = categoriesService.getList();
-        categoriesPromise.then(function(categories) {
+        categoriesService.getList().then(function(categories) {
             $scope.categories = categories;
         });
 
-        $scope.showAddPlaceForm = function(){
+        $scope.showAddPlaceForm = function() {
             $scope.form.name = $scope.query;
             $scope.addPlaceFormVisible = true;
             $scope.editAction = false;
         };
 
-        $scope.cancelAddPlaceForm = function(){
-            $scope.form = {};
+        $scope.cancelAddPlaceForm = function() {
+            $scope.closeForm();
             $scope.selectedCategory = $scope.defaultCategory;
-            $scope.addPlaceFormVisible = false;
-            $scope.clearForm();
             $scope.query = '';
             $scope.search();
         };
 
-        function clearFormAfterSave() {
-            $scope.clearForm();
-            $scope.addPlaceFormVisible = false;
+        $scope.clearFormAfterSave = function() {
+            $scope.closeForm();
             $scope.query = '';
             $scope.search();
             $scope.addPlaceFormBlock();
-        }
-
-        $scope.saveNewPlace = function(){
-
-            if($scope.form.category !== '') {
-
-                if(typeof $scope.form.web !== 'undefined') {
-                    if($scope.form.web.indexOf('http') < 0) {
-                        $scope.form.web = 'http://' + $scope.form.web;
-                    }
-                }
-
-                $scope.addPlaceFormDisable();
-                $scope.form.category = $scope.selectedCategory.id;
-
-                var addPlacePromise = placesService.post($scope.form)
-                addPlacePromise.then(function() {
-                    $modal.open({
-                        templateUrl: 'views/successModalMessage.tpl.html',
-                        controller: 'ModalInstanceCtrl',
-                        resolve: {
-                            data: function() {
-                                return {
-                                    title: 'ADD NEW PLACE',
-                                    text: 'Place was successfully added and now is awaiting approval from the administrators.'
-                                };
-                            }
-
-                        }
-                    });
-                    clearFormAfterSave();
-                }, function() {
-                    $modal.open({
-                        templateUrl: 'views/errorModalMessage.tpl.html',
-                        controller: 'ModalInstanceCtrl',
-                        resolve: {
-                            data: function() {
-                                return {
-                                    title: 'ADD NEW PLACE',
-                                    text: 'Adding the place failed. Please try again or contact the administrators.'
-                                };
-                            }
-                        }
-                    });
-                    $scope.addPlaceFormBlock();
-                });
-            } else {
-                $scope.selectedCategory = $scope.defaultCategory;
-
-            }
-        };
-
-        $scope.saveEditData = function(){
-
-            if($scope.form.web.indexOf('http') < 0) {
-                $scope.form.web = 'http://' + $scope.form.web;
-            }
-
-
-            placesService.one($scope.form.id).get().then(function(place) {
-                $scope.addPlaceFormDisable();
-
-                $scope.form = place;
-                place.category = $scope.selectedCategory.id;
-
-                var placePutPromise = place.put();
-                placePutPromise.then(function() {
-                    $modal.open({
-                        templateUrl: 'views/successModalMessage.tpl.html',
-                        controller: 'ModalInstanceCtrl',
-                        resolve: {
-                            data: function(){
-                                return {
-                                    title: 'EDIT PLACE',
-                                    text: 'Your suggestion was added and is now awaiting approval from the administrators.'
-                                };
-                            }
-
-                        }
-                    });
-                    clearFormAfterSave();
-                }, function() {
-                    $modal.open({
-                        templateUrl: 'views/errorModalMessage.tpl.html',
-                        controller: 'ModalInstanceCtrl',
-                        resolve: {
-                            data: function(){
-                                return {
-                                    title: 'EDIT PLACE',
-                                    text: 'Adding suggestion failed. Please try again or contact the administrators.'
-                                };
-                            }
-
-                        }
-                    });
-
-                    $scope.addPlaceFormBlock();
-                });
-            });
         };
 
         $scope.editItem = function(current) {
             $scope.selectedCategory = current.category;
-            $scope.form.id = current.id;
-            $scope.form.name = current.name;
-            $scope.form.town = current.town;
-            $scope.form.street = current.street;
-            $scope.form.web = current.web;
-            $scope.form.email = current.email;
-            $scope.form.phone = current.phone;
-            $scope.form.description = current.description;
+            $scope.form = current;
             $scope.form.category = current.category.id;
 
             $scope.editAction = true;
             $scope.addPlaceFormVisible = true;
         };
 
-        $scope.clearForm = function(){
+        $scope.closeForm = function(){
             $scope.form = {};
+            $scope.addPlaceFormVisible = false;
         };
     });
--- map/app/scripts/directives/search.js
+++ map/app/scripts/directives/search.js
@@ -1,16 +1,109 @@
 'use strict';
 
 angular.module('withloveApp')
-    .directive('appSearch', function() {
+    .directive('appSearch', function(placesService, $modal) {
         return {
             templateUrl: 'views/search.tpl.html',
             replace: true,
             controller: 'SearchCtrl',
-            link: function(scope, element, attrs) {
+            link: function(scope) {
+
+                var modalMessages = {
+                    add: {
+                        title: 'ADD NEW PLACE',
+                        success: 'Place was successfully added and now is awaiting approval from the administrators.',
+                        error: 'Adding the place failed. Please try again or contact the administrators.'
+                    },
+                    edit: {
+                        title: 'EDIT PLACE',
+                        success: 'Your suggestion was added and is now awaiting approval from the administrators.',
+                        error: 'Adding suggestion failed. Please try again or contact the administrators.'
+                    }
+                };
+
+                scope.saveNewPlace = function(){
+                    if(scope.form.category !== '') {
+
+                        if(typeof scope.form.web !== 'undefined') {
+                            if(scope.form.web.indexOf('http') < 0) {
+                                scope.form.web = 'http://' + scope.form.web;
+                            }
+                        }
+
+                        scope.addPlaceFormDisable();
+                        scope.form.category = scope.selectedCategory.id;
+
+                        placesService.post(scope.form).then(function() {
+                            $modal.open({
+                                templateUrl: 'views/successModalMessage.tpl.html',
+                                controller: 'ModalInstanceCtrl',
+                                resolve: {
+                                    data: function() {
+                                        return { title: modalMessages.add.title, text: modalMessages.add.success };
+                                    }
+                                }
+                            });
+                            scope.clearFormAfterSave();
+                        }, function() {
+                            $modal.open({
+                                templateUrl: 'views/errorModalMessage.tpl.html',
+                                controller: 'ModalInstanceCtrl',
+                                resolve: {
+                                    data: function() {
+                                        return { title: modalMessages.add.title, text: modalMessages.add.error };
+                                    }
+                                }
+                            });
+                            scope.addPlaceFormBlock();
+                        });
+                    } else {
+                        scope.selectedCategory = scope.defaultCategory;
+                    }
+                };
+
+                scope.saveEditData = function(){
+
+                    if(scope.form.web.indexOf('http') < 0) {
+                        scope.form.web = 'http://' + scope.form.web;
+                    }
+
+                    placesService.one(scope.form.id).get().then(function(place) {
+                        scope.addPlaceFormDisable();
+
+                        scope.form = place;
+                        place.category = scope.selectedCategory.id;
+
+                        var placePutPromise = place.put();
+                        placePutPromise.then(function() {
+                            $modal.open({
+                                templateUrl: 'views/successModalMessage.tpl.html',
+                                controller: 'ModalInstanceCtrl',
+                                resolve: {
+                                    data: function(){
+                                        return { title: modalMessages.edit.title, text: modalMessages.add.success };
+                                    }
+
+                                }
+                            });
+                            scope.clearFormAfterSave();
+                        }, function() {
+                            $modal.open({
+                                templateUrl: 'views/errorModalMessage.tpl.html',
+                                controller: 'ModalInstanceCtrl',
+                                resolve: {
+                                    data: function(){
+                                        return { title: modalMessages.add.title, text: modalMessages.add.error };
+                                    }
+
+                                }
+                            });
+                            scope.addPlaceFormBlock();
+                        });
+                    });
+                };
 
                 angular.element('.addplaceform-textarea').keyup(function() {
                     var maxLength = angular.element(this).attr('data-maxlength');
-
                     var numOfCharacters = angular.element(this).val().length;
 
                     if (angular.element(this).val().length > maxLength) {
